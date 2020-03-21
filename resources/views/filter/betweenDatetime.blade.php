d0f0b1e2a3a1758ab184d8b5b6d531bd95fd80e4
Muhlis Abdi <35951442+muhlisabdi@users.noreply.github.com>
2020-03-21 21:28:59 +0800

    fix typo

--- resources/views/filter/betweenDatetime.blade.php
+++ resources/views/filter/betweenDatetime.blade.php
@@ -2,14 +2,10 @@
     <label class="col-sm-2 control-label">{{$label}}</label>
     <div class="col-sm-8">
         <div class="input-group">
-            <div class="input-group-addon">
-                <i class="fa fa-calendar"></i>
-            </div>
-            <input type="text" class="form-control" id="{{$id['start']}}" placeholder="{{$label}}" name="{{$name['start']}}" value="{{ request()->input("{$column}.start", \Illuminate\Support\Arr::get($value, 'start')) }}>
+            <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+            <input type="text" class="form-control" id="{{$id['start']}}" placeholder="{{$label}}" name="{{$name['start']}}" value="{{ request()->input("{$column}.start", \Illuminate\Support\Arr::get($value, 'start')) }}">
             <span class="input-group-addon" style="border-left: 0; border-right: 0;">-</span>
-            <div class="input-group-addon" style="border-right: 0;">
-                <i class="fa fa-calendar"></i>
-            </div>
+            <span class="input-group-addon" style="border-right: 0;"><i class="fa fa-calendar"></i></span>
             <input type="text" class="form-control" id="{{$id['end']}}" placeholder="{{$label}}" name="{{$name['end']}}" value="{{ request()->input("{$column}.end", \Illuminate\Support\Arr::get($value, 'end')) }}">
         </div>
     </div>
